936263327cee0f82c5c9712e162c97755c7ddc2a
Unknown <rsakuma4@gmail.com>
2018-06-09 15:22:56 -0700

    song class now gets mp3 info

--- frontend/song.h
+++ frontend/song.h
@@ -22,17 +22,19 @@ using namespace std;
 class song
 {
 public:
-    song();
-    string getName();
+    song(string path_to_mp3);
+    string getTitle();
     string getArtist();
     string getAlbum();
+    string getGenre();
     string getPath();
 
 
 private:
-    QString name;
+    QString title;
     QString artist;
     QString album;
+    QString genre;
     QString path;
 
     TagLib::FileRef songf;
